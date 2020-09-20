4301c541f2b8cf47d03b2f19a7877f79af75aa86
Alexandra Spruit <alexandra.spruit@hotmail.com>
2020-09-20 20:52:51 +0200

    alexandra's functions added to preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,24 @@
+import numpy as np
 import pandas as pd
-tweets = pd.read_json('data.json')
+from spellchecker import SpellChecker
+import re
+
+# Initializes the spell checker and lammatizer.
+check = SpellChecker()
+
+# Reads in the data.
+data = pd.read_json('data.json')
 
 #reading only few for testing
-tweets = tweets[:5].copy()
+data = data[:5].copy()
 
-import re
+########################### FUNCTIONS ###################################
+def correct_text(text):
+# text needs to be a list of clean word tokens without other characters.
+    misspelled = check.unknown(text)
+    for word in misspelled:
+        text[text.index(word)] = check.correction(word)
+    return text
 
 # A function that extracts the hyperlinks from the tweet's content.
 def extract_link(text):
@@ -23,12 +37,19 @@ def word_in_text(word, text):
         return True
     return False
 
-tweets['link'] = tweets['text'].apply(lambda tweet: extract_link(tweet))
+######################### CLEANING ###########################################
+data['link'] = data['text'].apply(lambda tweet: extract_link(tweet))
 
 #tweets[tweets['link'] != ''].index
-for i in tweets[tweets['link'] != ''].index:
-    tweets['text'].loc[i] = re.sub(r"http\S+", "", tweets['text'].loc[i])
+for i in data[data['link'] != ''].index:
+    data['text'].loc[i] = re.sub(r"http\S+", "", data['text'].loc[i])
 
-del tweets['link']
+del data['link']
+
+# for row in dataframe:
+#     text_in_row = cleaned text1
+#     text_in_row = cleaned_text2
+
+data = data.replace("", np.nan).dropna()
 
 
